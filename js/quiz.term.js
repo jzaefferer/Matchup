2265ec4d43bbedf033d6ef45a0a50fbcf51527f2
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-07-30 14:59:24 +0200

    Cleanup textfill plugin, read starting fontsize from element

--- js/quiz.term.js
+++ js/quiz.term.js
@@ -1,23 +1,18 @@
 (function( $ ) {
 
-$.fn.textfill = function(options) {
-	var defaults = {
-		maxFontPixels: 40,
-		innerTag: 'span'
-	};
-	var Opts = jQuery.extend(defaults, options);
+$.fn.textfill = function() {
 	return this.each(function() {
-		var fontSize = Opts.maxFontPixels;
-		var ourText = $(Opts.innerTag + ':visible:first', this);
-		var maxHeight = $(this).height();
-		var maxWidth = $(this).width();
+		var ourText = $( this ).find( "span:visible:first" );
+		var fontSize = parseInt( ourText.css( "font-size" ), 10 );
+		var maxHeight = $( this ).height();
+		var maxWidth = $( this ).width();
 		var textHeight;
 		var textWidth;
 		do {
-			ourText.css('font-size', fontSize);
+			fontSize = fontSize - 1;
+			ourText.css( "font-size", fontSize );
 			textHeight = ourText.height();
 			textWidth = ourText.width();
-			fontSize = fontSize - 1;
 		} while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
 	});
 };
